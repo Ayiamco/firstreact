569cb596aff19f37d0c033fd3f0d2475e29c26c3
ayiamco <josephachonu@gmail.com>
2020-12-08 17:05:09 +0100

    refactor

--- src/App.js
+++ src/App.js
@@ -7,21 +7,72 @@ const url ="https://api.github.com/users"
 function App(){
 
     let [users,setUsers]=useState([])
+    let [screenSize,setScreenSize]=useState("");
+    let [isShowing,setIsShowing]=useState(true)
+    let [isLoading,setIsLoading]=useState(true)
 
     async function  getUsers(){
         let resp= await fetch(url);
         let jsonResp= await resp.json();
-        setUsers(jsonResp)
+        setUsers(jsonResp);
+        
         
     }
+    
     useEffect(()=>{
         getUsers()
+        setScreenSize(window.innerWidth)
+        setTimeout(()=>{
+            setIsLoading(false)
+        },10000)
+        console.log("User State was called")
+        
+        
     },[])
+
+    useEffect(()=>{
+        function resizeHandler(){
+            setScreenSize(window.innerWidth)
+        }
+
+        window.addEventListener("resize",resizeHandler)
+        // return ()=>{
+        //     window.removeEventListener('resize',resizeHandler)
+        // }
+    },[screenSize])
+
+    if(isLoading){
+        return (
+        <>
+            <h1>Exercise 2</h1>
+            <br></br>
+            <h3 style={{textAlign:"center"}}>Window Size: {screenSize}px</h3>
+            <button style={{
+                width:"20em",margin:"auto",
+                display:"block",padding:"0.5em",
+                marginBottom:"2em",cursor:"pointer"}}
+                className="btn" onClick={()=>{setIsShowing(!isShowing)}}>
+                Show/Hide Github Users
+            </button>
+           <h1>Loading Github Users............</h1>   
+        </>
+        )}
 
     return (
         <>
             <h1>Exercise 2</h1>
-            <h3>Github Users</h3>
+            <br></br>
+            <h3 style={{textAlign:"center"}}>Window Size: {screenSize}px</h3>
+            <button style={{
+                width:"20em",margin:"auto",
+                display:"block",padding:"0.5em",
+                marginBottom:"2em",cursor:"pointer"}}
+                className="btn" onClick={()=>{setIsShowing(!isShowing)}}>
+                Show/Hide Github Users
+            </button>
+        
+           {isShowing ? <div>
+            <h3 style={{textAlign:"center",marginBottom:"-2em"}}>Github Users</h3>
             <div className="user-grid">
                 {
                 users.map((user)=>{
@@ -30,6 +81,9 @@ function App(){
                 })
             }
             </div>
+           </div>: <h1>Users are hidden</h1>
+           } 
+            
             
         </>
     )
