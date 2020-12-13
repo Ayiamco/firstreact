e9e7ec3f19807cdec85efd8b06664d007ced4188
ayiamco <josephachonu@gmail.com>
2020-12-13 13:49:32 +0100

    refactor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router,Route,Switch} from "react-router-dom"
+import {BrowserRouter as Router,Route} from "react-router-dom"
 
 import Exercise1 from "./js/Exercise1"
 import Exercise2 from "./js/Exercise2"
